d703dab65c32615cee89c21c516afdbdcc05c5a2
Wicked001 <ca55per@gmail.com>
2017-08-23 03:22:52 -0400

    Initial Commit
    Working express node.js friend finder app.  Takes input from the user and compares it to the existing data to find the closest match.  Returns the match in a modal and displays the profile image and username.

---
+++ data/friends.js
@@ -0,0 +1,48 @@
+module.exports = [{
+  "name":"Positive Joe",
+  "photo":"https://s-media-cache-ak0.pinimg.com/originals/2f/e0/56/2fe056dc227cbe938807b0a6367ff457.gif",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+},
+{
+  "name":"Middle of the Road Bob",
+  "photo":"https://images.genius.com/8c63fb9a41d63fd887a6012af7b09b35.500x281x20.gif",
+  "scores":[
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+},
+{
+  "name":"Negative Nancy",
+  "photo":"https://cdn2.scratch.mit.edu/get_image/gallery/1276718_170x100.png?v=1433206276.79",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+}]
